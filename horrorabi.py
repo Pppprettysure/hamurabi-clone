05d2abc681ca527ce502f6e955b472d63e3eca37
Jessie <51302382+Pppprettysure@users.noreply.github.com>
2019-06-04 04:12:08 -0400

    Build structure for displaying and storing gamestate

---
+++ horrorabi.py
@@ -0,0 +1,46 @@
+# SET UP 
+
+# Year use as for loop variable
+
+GAME_SUMMARY =  ( 
+       "In year {year}:\n"
+       "{starved} people starved, and {immigrants} came to the city.\n"
+       "The population is now {population}.\n"
+       "The city owns {acres} acres.\n"
+       "We harvested {harvest} bushels per acre.\n"
+       "Rats ate {grain_eaten} bushels.\n"
+       "You now have {grain} bushels in storage.\n"
+       "Land is trading at {land_value} bushels per acre.\n" 
+        ) 
+
+game_state = dict.fromkeys([
+        "year", "starved", "immigrants", "population", "acres", 
+        "harvest", "grain_eaten", "grain", "land_value"
+        ])
+
+name = ""
+
+# TODO: FIGURE OUT A BETTER WAY TO DEAL WITH THIS
+def game_init():
+    """Initialize variables to default states prior to play."""
+    game_state["year"] = 1
+    game_state["starved"] = 0
+    game_state["immigrants"] = 5
+    game_state["population"] = 100
+    game_state["acres"] = 1000
+    game_state["harvest"] = 3
+    game_state["grain_eaten"] = 200
+    game_state["grain"] = 2800
+    game_state["land_value"] = 26
+    name = input("What is your name, ruler? ")
+
+game_init()
+# CORE LOOP
+
+while game_state["year"] < 11:
+    # Output
+    print(GAME_SUMMARY.format(**game_state)) 
+    game_state["year"] += 1
+
+# SHUT DOWN
+
