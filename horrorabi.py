850fca52c3fc1b743a84dd773741de1dc090b214
Jessie <51302382+Pppprettysure@users.noreply.github.com>
2019-06-05 16:56:08 -0400

    Clean up code

--- horrorabi.py
+++ horrorabi.py
@@ -2,8 +2,10 @@
 
 import random
 
-# Year use as for loop variable
-
+game_state = dict.fromkeys([
+        "year", "starved", "immigrants", "population", "acres", 
+        "harvest", "grain_eaten", "grain", "land_value"
+        ])
 GAME_SUMMARY =  ( 
        "In year {year}:\n"
        "{starved} people starved, and {immigrants} came to the city.\n"
@@ -14,17 +16,10 @@ GAME_SUMMARY =  (
        "You now have {grain} bushels in storage.\n"
        "Land is trading at {land_value} bushels per acre.\n" 
         ) 
-
-game_state = dict.fromkeys([
-        "year", "starved", "immigrants", "population", "acres", 
-        "harvest", "grain_eaten", "grain", "land_value"
-        ])
-
 name = ""
 
-# TODO: FIGURE OUT A BETTER WAY TO DEAL WITH THIS
 def game_init():
-    """Initialize variables to default states prior to play."""
+    """Initialize variables to initial states prior to play."""
     game_state["year"] = 1
     game_state["starved"] = 0
     game_state["immigrants"] = 5
@@ -36,20 +31,23 @@ def game_init():
     game_state["land_value"] = 26
     name = input("What is your name, ruler? ")
 
-game_init()
 random.seed()
+game_init()
+
 # CORE LOOP
 
 while game_state["year"] < 11:
-    # Output
+    # OUTPUT
     print(GAME_SUMMARY.format(**game_state))
     
-    # Input
+    # INPUT
 
-    # Processing
+    # PROCESSING
+
+    # Generate land value
     game_state["land_value"] = random.randint(17, 26)
 
-    # IS TOO MUCH GRAIN EATEN? BALANCE ONCE GAME IS WORKING
+    # Determine how much grain was eaten
     game_state["grain_eaten"] = random.randint(0,
             game_state["grain"] // 200) * 100
     game_state["grain"] -= game_state["grain_eaten"]
@@ -58,4 +56,3 @@ while game_state["year"] < 11:
     game_state["year"] += 1
 
 # SHUT DOWN
-
